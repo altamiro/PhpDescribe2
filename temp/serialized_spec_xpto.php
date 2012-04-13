6d489ed74d9f6ef10c05a8995cf5b610370939d2
Bruno Reis <bruno.p.reis@gmail.com>
2012-04-13 19:22:34 -0300

    testing SingleSpecRunner serialization of specs on flat_tests.php. Refactoring isolationg SingleSpecRunner in it's own file.

---
+++ temp/serialized_spec_xpto.php
@@ -0,0 +1,4 @@
+<?php
+ $___spec_ = unserialize('O:4:"Spec":6:{s:10:"' . "\0" . 'Spec' . "\0" . 'name";s:12:"do something";s:10:"' . "\0" . 'Spec' . "\0" . 'code";s:19:"$a=2;
+$a = $a + 1;
+";s:15:"' . "\0" . 'Spec' . "\0" . 'sub_specs";a:0:{}s:12:"' . "\0" . 'Spec' . "\0" . 'result";N;s:13:"' . "\0" . 'Spec' . "\0" . 'message";N;s:16:"' . "\0" . 'Spec' . "\0" . 'error_line";N;}'); 
